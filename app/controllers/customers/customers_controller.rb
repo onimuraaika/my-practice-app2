61b44ea7b331d6af567c768d76d1fdefeb5bc7be
onimuraaika <aika.onimura@gmail.com>
2021-01-26 06:20:48 +0000

    [Fix]deviseの画面が表示されるようにコントローラ・ルーティングを修正

---
+++ app/controllers/customers/customers_controller.rb
@@ -0,0 +1,11 @@
+class Customers::CustomersController < ApplicationController
+  # before_action :authenticate_customer!
+  # before_action :set_current_customer
+
+  def show
+  end
+  
+  # def customer_params
+  #   params.require(:customer).permit(:name)
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,23 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'homes/top'
-  end
-  devise_for :admins
-  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # devise_for :admins, controllers: {
-  #   sessions: 'admins/sessions',
-  # }
+  # 管理者
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+  }
   
-  # namespace :admin do
-  #   get 'top' => 'homes#top', as: 'top'
-  # end
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+  end
   
-  # devise_for :customers, controllers: {
-  #   sessions: 'customers/sessions',
-  #   registrations: 'customers/registrations',
-  # }
+  # 顧客
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    registrations: 'customers/registrations',
+  }
   
   scope module: :customers do
     root 'items#top'
-    # get 'customers/mypage' => 'customers#show', as: 'mypage'
+    get 'customers/mypage' => 'customers#show', as: 'mypage'
   end
-  
-  # get 'customers/about' => 'homes#about'
 end
