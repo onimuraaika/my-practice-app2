1c6877f7b195131fbabde3c88dbaf902e3fca219
onimuraaika <aika.onimura@gmail.com>
2021-01-27 06:39:26 +0000

    [Add]Itemモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_035008) do
+ActiveRecord::Schema.define(version: 2021_01_27_051757) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,19 @@ ActiveRecord::Schema.define(version: 2021_01_27_035008) do
     t.string "profile_image_id"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image_id", null: false
+    t.string "introduction", null: false
+    t.integer "price", null: false
+    t.integer "purpose", default: 0, null: false
+    t.integer "for_someone", default: 0, null: false
+    t.integer "genger", default: 0, null: false
+    t.integer "age", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
