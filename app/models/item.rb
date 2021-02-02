cf3e513ae1cb5d1d6766d94a4f0621b68188b3ef
onimuraaika <aika.onimura@gmail.com>
2021-02-02 07:36:09 +0000

    [Add]値段の選択肢を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   attachment :image
   
+  enum price: { price1000: 0, price5000: 1, price10000: 2 } 
   enum purpose: { general: 0, birthday: 1, anniversary: 2, valentine: 3, christmas: 4 }
   enum for_someone: { everyone: 0, friend: 1, lover: 2, family: 3, baby: 4 }
   enum gender: { both: 0, male: 1, female: 2 }
